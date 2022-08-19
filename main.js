2bc861f7ebf347c04a9b84813bb5f05c20e1ed78
Pete Fowler <fowler.pete@gmail.com>
2022-08-19 12:29:56 -0600

    Add title, date, description of IOD
    Added to main-bar and faded in, needs additional style

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const main = document.querySelector('#main');
+const mainBar = document.querySelector('#main-bar');
 
 // Gets image of the day
 function getIOD () {
-  return fetch(`https://api.nasa.gov/planet***ary/apod?api_key=DEMO_KEY`)
+  return fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
   .then(res => res.json())
   .then(data => {
     console.log(data);
     displayIOD(data);
+    displayBarIOD(data)
   });
 }
 
+// Displays image of the day in main section
 function displayIOD(data) {
   main.setAttribute('style', `background: url(${data.url}`);
 }
+
+// Adds data to populate main-bar at the bottom of the image and fades it in
+function displayBarIOD(data) {
+  const title = document.createElement('span');
+  title.id = 'title';
+  title.textContent = data.title;
+  
+  const date = document.createElement('span');
+  date.id = 'date';
+  title.textContent = data.date;
+  
+  const description = document.createElement('p');
+  description.id = 'description';
+  description.textContent = data.explanation;
+
+  // Append new elements to DOM and fades in the main bar by removing the 
+  // CSS class .faded-out (which has set the element's opacity to 0)
+  mainBar.append(title, date, description);
+  mainBar.classList.remove('faded-out');
+}
+
 getIOD();
