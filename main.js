60d44bd368311a284e7b9b33392f4bf20f903951
Pete Fowler <fowler.pete@gmail.com>
2022-08-19 11:56:05 -0600

    Get image of day

---
+++ main.js
@@ -0,0 +1,16 @@
+const main = document.querySelector('#main');
+
+// Gets image of the day
+function getIOD () {
+  return fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    displayIOD(data);
+  });
+}
+
+function displayIOD(data) {
+  main.setAttribute('style', `background: url(${data.url}`);
+}
+getIOD();
