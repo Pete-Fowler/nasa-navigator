c43c666405d06803bc0faf3f4c11b259b74a3a35
Pete Fowler <fowler.pete@gmail.com>
2022-08-23 09:16:11 -0600

    Improve modal window
    Stay hidden on Mars screen

--- main.js
+++ main.js
@@ -16,13 +16,65 @@ const title = document.querySelector('#title');
 const description = document.querySelector('#description');
 
 const expand = document.querySelector('#expand');
-expand.addEventListener('mouseover', displayDetails);
-expand.addEventListener('mouseout', hideDetails);
 
 let localMarsData;
 let localData;
 
-// Search images API
+getIOD();
+
+// Image of the day functions
+function getIOD () {
+  return fetch(`https://api.nasa.gov/planetary/apod?api_key=k7cUJwa1gGIvD71WgzJVCjVdErEJWvQQX7aL9htz`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    localData = data;
+    displayIOD(data);
+  });
+}
+
+function displayIOD(data) {
+  main.textContent = '';
+  expand.setAttribute('style', 'opacity: .4');
+  expand.addEventListener('mouseover', displayDetails);
+  expand.addEventListener('mouseout', hideDetails);
+  if(data.media_type === 'image') {
+    main.setAttribute('style', `background-image: url(${data.url}`);
+  }
+  else if(data.media_type === 'video') {
+    const video = document.createElement('iframe');
+    video.src = data.url;
+    main.append(video);
+  }
+  console.log(title);
+  title.textContent = `Title: ${data.title} | Image Date: ${data.date}`;
+  description.textContent = data.explanation;
+}
+
+// Mars button functions
+function getMars() {
+  return fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=navcam&api_key=DEMO_KEY")
+  .then(res => res.json())
+  .then(data => {
+    localMarsData = {...data};
+    console.log(data);
+    displayMars(data);
+  })
+}
+
+function displayMars(data) {
+  main.textContent = "";
+  expand.setAttribute('style', 'opacity: 0');
+  expand.removeEventListener('mouseover', displayDetails);
+  expand.removeEventListener('mouseout', hideDetails);
+  element = Math.round(Math.random() * 9)
+  main.setAttribute("style", `background-image: url(${data.photos[element].img_src}`)
+  
+  roverName = data.photos[element].rover.name;
+  currentView.textContent = `Mars images captured by the rover ${roverName}`;
+  }
+
+// Search images API functions
 function search(string) {
   return fetch(`https://images-api.nasa.gov/search?q=${string}`)
   .then(res => res.json())
@@ -50,38 +102,14 @@ function displaySearchResults(data) {
   }
 
 function displaySearchDetail(object) {
+  expand.setAttribute('style', 'opacity: .4')
+  expand.addEventListener('mouseover', displayDetails);
+  expand.addEventListener('mouseout', hideDetails);
   title.textContent = object.title;
   description.textContent = object.description;
 }
 
-// Gets image of the day
-function getIOD () {
-  return fetch(`https://api.nasa.gov/planetary/apod?api_key=k7cUJwa1gGIvD71WgzJVCjVdErEJWvQQX7aL9htz`)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    localData = data;
-    displayIOD(data);
-  });
-}
-
-// Displays image of the day in main section
-function displayIOD(data) {
-  main.textContent = '';
-  if(data.media_type === 'image') {
-    main.setAttribute('style', `background-image: url(${data.url}`);
-  }
-  else if(data.media_type === 'video') {
-    const video = document.createElement('iframe');
-    video.src = data.url;
-    main.append(video);
-  }
-  console.log(title);
-  title.textContent = `Title: ${data.title} | Image Date: ${data.date}`;
-  description.textContent = data.explanation;
-}
-
-// Adds data to populate main-bar at the bottom of the image and fades it in
+// Show / hide modal window
 function displayDetails() {
   mainBar.style.opacity = 0;
  
@@ -93,27 +121,3 @@ function hideDetails() {
   mainBar.style.opacity = 0;
   mainBar.classList.add('faded-out');
 }
-
-getIOD();
-
-function getMars() {
-  return fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=navcam&api_key=DEMO_KEY")
-  .then(res => res.json())
-  .then(data => {
-    localMarsData = {...data};
-    console.log(data);
-    displayMars(data);
-  })
-}
-
-function displayMars(data) {
-  main.textContent = "";
-  element = Math.round(Math.random() * 9)
-  main.setAttribute("style", `background-image: url(${data.photos[element].img_src}`)
-  
-  roverName = data.photos[element].rover.name;
-  currentView.textContent = `Mars images captured by the rover ${roverName}`;
-
-  }
-
-
